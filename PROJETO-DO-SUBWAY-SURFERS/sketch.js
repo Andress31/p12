febdda080ecb5ecf6834abc43e0675f9d4122bbc
Andress31 <130068953+Andress31@users.noreply.github.com>
2023-04-13 11:15:57 -0300

    Add files via upload

---
+++ PROJETO-DO-SUBWAY-SURFERS/sketch.js
@@ -0,0 +1,36 @@
+//é aqui que cria as variáveis
+var jogadoranimaçao, jogador;
+var estradaimagem, estradaSprite;
+function preload(){
+  //é aqui que carrega a imagem
+ estradaimagem = loadImage("estrada.png");
+  //é aqui que carrega a animação
+ jogadoranimaçao = loadAnimation ("Jake1.png","Jake2.png","Jake3.png","Jake4.png","Jake5.png");
+}
+
+function setup(){
+  createCanvas(600,400)
+  //é aqui que cria a sprite da estrada
+  estrada = createSprite (300,200);
+  //é aqui que adiciona a imagem na sprite da estrada
+estrada.addImage(estradaimagem);
+  //é aqui que define a velocidade
+estrada.velocityY = 10;
+
+  //é aqui que cria a sprite do jogador
+jogador = createSprite(300,300);
+  //é aqui que adiciona a animação do jogador na sprite
+jogador.addAnimation("correndo",jogadoranimaçao);
+  //é aqui que define o tamanho
+jogador.scale = 0.5
+}
+
+function draw(){
+  background(0);
+
+  //programar para a sprite de estrada estar em um loop infinito
+if(estrada.y > 400){
+estrada.y = 300;
+}   
+  drawSprites();
+}
